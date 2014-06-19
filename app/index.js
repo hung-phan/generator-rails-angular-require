6fd0819cc42024ecf60d7a7aae1cfa363c0c952d
hung-phan <colorvisavn@gmail.com>
2014-06-19 17:03:52 +0700

    update with template

--- app/index.js
+++ app/index.js
@@ -41,6 +41,29 @@ var RailsAngularRequireGenerator = yeoman.generators.Base.extend({
     }.bind(this));
   },
 
+  assForTemplate: function() {
+    var cb = this.async();
+
+    var prompts = [{
+      type: 'checkbox',
+      name: 'templateFile',
+      message: 'What template support would you like to include?',
+      choices: [
+        { name: 'HAML' , value: 'includeHaml' , checked: true },
+        { name: 'SLIM' , value: 'includeSlim' , checked: true }
+      ]
+    }];
+
+    this.prompt(prompts, function (props) {
+      function includeTemplate(template) { return props.templateFile.indexOf(template) !== -1; }
+
+      // template support
+      this.includeHaml = includeTemplate('includeHaml');
+      this.includeSlim = includeTemplate('includeSlim');
+
+      cb();
+    }.bind(this));
+  },
 
   assForJSFile: function() {
     var cb = this.async();
