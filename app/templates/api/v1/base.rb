fc34f89e6dea2dff21a83a52f13fe06df4919980
hung-phan <colorvisavn@gmail.com>
2014-06-29 13:45:23 +0700

    update grape

--- app/templates/api/v1/base.rb
+++ app/templates/api/v1/base.rb
@@ -2,7 +2,7 @@ module API
   module V1
     class Base < Grape::API
       # base api for v1
-      mount API::V1::ObjectApi
+      mount API::V1::PersonApi
     end
   end
 end
---
+++ app/templates/api/v1/person_api.rb
@@ -0,0 +1,40 @@
+module API
+  module V1
+    class PersonApi < Grape::API
+      include API::V1::Defaults
+
+      helpers do
+        def current_user
+          true
+        end
+
+        def authenticate!
+          error!('401 Unauthorized', 401) unless current_user
+        end
+      end
+
+      # GET /api/v1/people
+
+      resource :people do
+        # GET /api/v1/people
+        desc "Get all people"
+        get "/" do
+          Person.all
+        end
+
+        # GET /api/v1/people/:id
+        desc "Retrieve person with id"
+        get "/:id" do
+          Person.find(params[:d])
+        end
+
+        # POST /api/v1/people/create
+        desc "Create person"
+        post "/create" do
+          MyObject.create(params[:data])
+        end
+      end
+    end
+  end
+end
+
